54c249e6c152171ae694f47b5adee26bc6e828df
kubiteddie <eddiekubit0@gmail.com>
2022-11-14 16:52:49 -0500

    Commented out unnecessary testing

--- test/helpers/application_helper_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ApplicationHelperTest < ActionView::TestCase
-    test "full title helper" do
-        assert_equal FILL_IN, full_title
-        assert_equal FILL_IN, full_title("Help")
-    end
-end
